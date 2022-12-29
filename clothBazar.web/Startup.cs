41a8ad7373455d637c7455a7c397ec0c2fbb8e6e
Abrar <abrarkhalifa63@gmail.com>
2022-12-29 11:33:45 +0530

    Add project files.

---
+++ clothBazar.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(clothBazar.web.Startup))]
+namespace clothBazar.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
